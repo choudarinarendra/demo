cd343909bb21fd8a7d0a11d64346bbc1adb4e9f8
Narendra Choudari <narendrachoudari00@gmail.com>
2023-06-15 11:53:55 +0530

    LoginPage modified

--- LoginPage/loginpage.js
+++ LoginPage/loginpage.js
@@ -23,4 +23,33 @@ form.addEventListener("submit" , (e) => {
    
    let json = JSON.stringify(obj)
    console.log(json);
+   let fe=fetch("http://localhost:8080/users/login",{
+        method: "POST", 
+       
+        headers: {
+            "Content-Type": "application/json",
+           
+          },
+       
+          body: json,
+       })
+       fe.then((x)=>x.json())
+       .then((x)=>{
+        console.log(x);
+         if(x.id!==0&&x.phno!==0&&x.name!==null){
+        sessionStorage.setItem("name",x.name)
+        sessionStorage.setItem("email",x.email)
+        sessionStorage.setItem("password",x.password)
+        sessionStorage.setItem("phno",x.phno)
+        sessionStorage.setItem("id",x.id)
+        window.open("../UserPage/userpage.html")
+         }
+         else{
+          let emailValue=document.getElementById("log_email")
+          let passwordValue=document.getElementById("log_pass")
+         }
+       })
+       .catch((x)=>{
+        console.log(x);
+       })
 })
